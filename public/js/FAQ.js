2ef367791b5dec18985bcc0bc4d611ac9725e59c
Nguyen <tvngiagia@gmail.com>
2019-01-07 11:57:38 +0700

    zzz

--- public/js/FAQ.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-	$('.carousel-inner').on('click', function(e) {
-		$(this).toggleClass("max-height"); //you can list several class names 
-		e.preventDefault();
-    });
-});
-
