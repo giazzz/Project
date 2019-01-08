fabe338e73a6fd40b3293a837b8e5dcc71581d41
Pham Anh Dung <anhdungpham090@gmail.com>
2019-01-08 21:09:55 +0700

    Thêm comment cho trang Detail.html
    Code sửa trong 2 file detail.html và detail.css
    tìm code của tôi bằng cách find cụm từ:  đây là code của Dũng

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,89 +1,240 @@
-var app = angular.module("myApp",["ngRoute","ngAnimate"]);
-
+var app = angular.module("myApp",["ngRoute","ngAnimate","angularUtils.directives.dirPagination","ngCookies"]);
 
 app.config(['$routeProvider',function($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl : "dashboard.html"
-    })
-    .when("/loai", {
-       templateUrl : "loai.html",
-       controller: "loaiController"
-    })
-    .when("/sp", {
-       templateUrl : "sp.html",
-       controller: "spController"
-    })
-    .otherwise({ redirectTo: '/' });
-}]);
+  $routeProvider
+    // .when("/them-shop/:so", {
+    //     templateUrl : "http://localhost:3003/Project-caominhquan/list-ghep-header-footer.html"
+    // })
+    // .when("/loai", {
+    //    templateUrl : "loai.html",
+    //    // controller: "loaiController"
+    // })
+    // .when("/sp", {
+    //    templateUrl : "sp.html",
+    //    // controller: "spController"
+    // })
+    // .when("/themsp", {
+    //     templateUrl : "themsp.html"
+    // })
+    // .when("/themloai", {
+    //     templateUrl : "themloai.html"
+    // })
+    // .when("/editproduct", {
+    //     templateUrl : "editproduct.html"
+    // })
+    // .when("/edittype.html/", {
+    //     templateUrl : "edittype.html"
+    // })
+    // .otherwise({ redirectTo: '/' });
+  }]);
 
-app.controller("dataController",function($scope,$rootScope,$routeParams,$http){
-    $http.get('/typeedit').
-        then(function(result, status, headers, config) {
-          console.log('da get api');
-          console.log(result);
-          $rootScope.edits = result.data.data;
+app.controller("dataController",function($scope,$rootScope,$routeParams,$http,$cookies,$cookieStore){
+  $rootScope.pageSize = 9;
+  $rootScope.currentPage = 1;
+  $scope.filters = { };
+  $scope.model = {};
+
+  $scope.sortType     = 'name'; // set the default sort type
+  $scope.sortReverse  = false;  // set the default sort order
+
+
+    $http.get('/all_product').
+    then(function(result, status, headers, config) {
+      $rootScope.products = result.data.data;
+          // console.log(result);
         });
-});
-
-
-app.controller("loaiController",function($scope,$rootScope,$routeParams,$http){
-            $http.get('/type').
-                then(function(result, status, headers, config) {
-                  console.log('da get api');
-                  console.log(result);
-                  $rootScope.types = result.data.data;
-                });
-              
-
-});
-
-app.controller("spController",function($scope,$rootScope,$routeParams,$http){
-            $http.get('/all_product').
-                then(function(result, status, headers, config) {
-                  // console.log('da get api');
-                  // console.log(result);
-                  $rootScope.products = result.data.data;
-                });
-  var dataFromMogo = "http://localhost:3003/all_product";
-
-  $scope.chuyen = function(doituong){
-      doituong.hienra = !doituong.hienra;
-  };
-      // ham nut save
-      $scope.sua = function(doituong){
-        doituong.hienra = !doituong.hienra;
-      //lay du tu form
-      var dulieucansua = $.param({
-          id:doituong.id,
-          name:doituong.name,
-          price:doituong.price,
-          type:doituong.type,
-          desc:doituong.desc,
-          comment:doituong.comment,
-          rating:doituong.rating,
-          image_link:doituong.image_link
+    $http.get('/type').
+    then(function(result, status, headers, config) {
+      $rootScope.types = result.data.data;
+    });
+    $http.get('/user').
+    then(function(result, status, headers, config) {
+      $rootScope.users = result.data.data;
+          // console.log(result);
+        });
+    $http.get('/shop_product').
+    then(function(result, status, headers, config) {
+      $rootScope.shops = result.data.data;
+        // console.log(result);
       });
-      console.log(dulieucansua);
-      var urlAPI = 'http://localhost:3003/sua-products';
-      var config ={
-        headers:{
-          'content-type':'application/x-www-form-urlencoded;charset=utf-8'
+
+    $http.get('/chi_tiet_product').
+    then(function(result, status, headers, config) {
+      $rootScope.ones = result.data.data;
+      // console.log(result);
+    });
+    $http.get('/hot_product').
+    then(function(result, status, headers, config) {
+      $rootScope.hots = result.data.data;
+      // console.log(result);
+    });
+    $http.get('/random_product').
+    then(function(result, status, headers, config) {
+      $rootScope.randoms = result.data.data;
+      // console.log(result);
+    });
+    $http.get('/random_4_product').
+    then(function(result, status, headers, config) {
+      $rootScope.rd = result.data.data;
+      // console.log(result);
+    });
+
+    
+    // ADD TO CART
+    $scope.cart = [];
+    $scope.total = 0;
+    
+    //Tao cookies cho sp va bien total
+    if(!angular.isUndefined($cookies.get('total'))){
+      $scope.total = parseFloat($cookies.get('total'));
+    }
+    //
+    if (!angular.isUndefined($cookies.get('cart'))) {
+      $scope.cart =  $cookies.getObject('cart');
+    }
+    // Them sp vao shop
+    $scope.addItemToCart = function(product){  
+      if ($scope.cart.length === 0){
+        product.count = 1;
+        $scope.cart.push(product);
+      } else {
+        var repeat = false;
+        for(var i = 0; i< $scope.cart.length; i++){
+          if($scope.cart[i].id === product.id){
+            repeat = true;
+            $scope.cart[i].count +=1;
+          }
+        }
+        if (!repeat) {
+          product.count = 1;
+          $scope.cart.push(product);  
         }
       }
-      $http.post(urlAPI,dulieucansua,config)
-      .then(function(res){
-          console.log(res.data);
-      },function(er){
-          console.log(er.data);
-      })
-      //luu vao CSDL
-      // $http.post(dataFromMogo,dulieucansua,{headers:{
-      //   'content-type':'application/x-www-form-urlencoded;charset=utf-8'
-      // }}).then(function(res){
-      //     console.log(res.data);
-      // },function(er){
+      var expireDate = new Date();
+      expireDate.setDate(expireDate.getDate() + 1);
+      $cookies.putObject('cart', $scope.cart,  {'expires': expireDate});
+      $scope.cart = $cookies.getObject('cart');
 
-      // })
-  };            
-});
+      $scope.total += parseFloat(product.price);
+      $cookies.put('total', $scope.total,  {'expires': expireDate});
+    };
+
+     //Tao cookies luu so luong sp trc khi add to cart(Trang Detail)
+     if(!angular.isUndefined($cookies.get('slsp'))){
+      $scope.slsp = $cookies.get('slsp');
+    }
+    
+    $scope.qty_incr = function(item){
+      item.count = item.count + 1;
+      $scope.slsp = item.count;
+      expireDate = new Date();
+      expireDate.setDate(expireDate.getDate() + 1);
+      $cookies.put('slsp', $scope.slsp,  {'expires': expireDate});  
+    }
+    $scope.qty_decr = function(item){
+      if(item.count > 1){
+        item.count = item.count - 1;
+        $scope.slsp = item.count;
+        expireDate = new Date();
+        expireDate.setDate(expireDate.getDate() + 1);
+        $cookies.put('slsp', $scope.slsp,  {'expires': expireDate});  
+      }
+
+    }
+    // Them sp da set quantity vao shop (Trang Detail)
+    $scope.addItemquantity = function(product){ 
+      var check = false; 
+      //Kiem tra da add to cart chua? trc khi vao trang detail
+      for(var i = 0; i< $scope.cart.length; i++){
+        if($scope.cart[i].id === product.id){
+          check = true;
+          $scope.cart[i].count += $scope.slsp;
+          expireDate = new Date();
+          expireDate.setDate(expireDate.getDate() + 1);
+          $cookies.putObject('cart', $scope.cart, {'expires': expireDate});
+          $scope.cart = $cookies.getObject('cart');
+          $scope.total += parseFloat(product.price*product.count);
+          $cookies.put('total', $scope.total,  {'expires': expireDate});
+        }
+      }
+      console.log(check);
+      //Truong hop chua add to cart trc khi vao trang detail
+      if(check == false){
+        product.count = $scope.slsp;
+
+        $scope.cart.push(product);
+
+        var expireDate = new Date();
+        expireDate.setDate(expireDate.getDate() + 1); 
+        $cookies.putObject('cart', $scope.cart, {'expires': expireDate});
+        $scope.cart = $cookies.getObject('cart');
+        $scope.total += parseFloat(product.price*product.count);
+        $cookies.put('total', $scope.total,  {'expires': expireDate});
+
+      }
+    };
+
+     // XOA HAN SP
+     $scope.removeItemCart = function(product){
+      var index = $scope.cart.indexOf(product);
+      $scope.cart.splice(index, 1);
+      expireDate = new Date();
+      expireDate.setDate(expireDate.getDate() + 1);
+      $cookies.putObject('cart', $scope.cart, {'expires': expireDate});
+      $scope.cart = $cookies.getObject('cart');
+
+      $scope.total -= parseFloat(product.price*product.count);
+      $cookies.put('total', $scope.total,  {'expires': expireDate});
+
+    };
+     // Xoa tung sp mot
+     $scope.removeONE = function(product){
+
+       if(product.count > 1){
+         product.count -= 1;
+         var expireDate = new Date();
+         expireDate.setDate(expireDate.getDate() + 1);
+         $cookies.putObject('cart', $scope.cart, {'expires': expireDate});
+         $scope.cart = $cookies.getObject('cart');
+         $scope.total -= parseFloat(product.price);
+         $cookies.put('total', $scope.total,  {'expires': expireDate});
+       }   
+     };
+     // Luu ten loai sp cho trag chi tiet(de lay cac sp cung loai)
+     $scope.loaisp = "";
+     if(!angular.isUndefined($cookies.get('loaisp'))){
+      $scope.loaisp = $cookies.get('loaisp');
+    }
+    $scope.spChitiet = function(product){
+      $scope.loaisp = product.type;
+      expireDate = new Date();
+      expireDate.setDate(expireDate.getDate() + 1);
+      $cookies.put('loaisp', $scope.loaisp,  {'expires': expireDate});      
+    };
+    //Chon phuong thuc thanh toan
+    $scope.pay = "";
+    if(!angular.isUndefined($cookies.get('pay'))){
+      $scope.pay = $cookies.get('pay');
+    }
+
+    $scope.payment = function(){
+      expireDate = new Date();
+      expireDate.setDate(expireDate.getDate() + 1);
+      $cookies.put('pay', $scope.pay,  {'expires': expireDate}); 
+    }
+    // $scope.xemgiatri = function(){
+    //   console.log(pay);
+    // }
+
+
+    $scope.delete_all_cookies = function(){
+      var cookies = $cookies.getAll();
+      angular.forEach(cookies, function (v, k) {
+        $cookies.remove(k);
+      });
+    };
+
+
+
+  });
+
