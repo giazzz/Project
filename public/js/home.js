f4814bedd9a54842c1f1ac5565934dfbaac6f08f
giazzz <nguyen128225>
2019-01-08 14:49:34 +0700

    zzz

--- public/js/home.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-	$( ".product-action" ).click(function() {
-		$(this).find('.addtocart').hide();
-		$(this).css( "background-color", "white" );
-		$(this).find('.viewcart').slideDown(300);
-	});
-});	
-	
